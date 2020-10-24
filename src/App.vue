9b48443e5d20bd6c4349b12aeb86799d234872e3
马旭辉 <maxuhui@hornsun.com>
2020-10-24 10:00:53 +0800

    优化

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,5 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link>
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view/>
   </div>
 </template>
@@ -20,7 +16,3 @@ export default {
   },
 };
 </script>
-
-<style lang="scss">
-
-</style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="hello">
-    <a-button v-on:click="getData">发送请求</a-button>
+    <a-button v-on:click="getData" type="primary">发送请求</a-button>
+    <a-button v-on:click="cleanData" type="danger">清空数据</a-button>
+    <p v-for="(item,index) of this.list" v-bind:key="index">
+      {{ item.projectName }}
+    </p>
   </div>
 </template>
 
@@ -8,14 +12,23 @@
 export default {
   name: 'HelloWorld',
   data: function() {
-    return {};
+    return {
+      list: [],
+    };
+  },
+  setup: function() {
   },
   inject: ['http', 'api'],
   methods: {
     getData: function() {
-      this.http.post(this.api.project.list, {pageNumber: 1}).then((response)=>{
-        console.log(response.data);
+      this.http.post(this.api.project.list,
+          {pageNumber: 1},
+      ).then((response) => {
+        this.list = response.data.list;
       });
+    },
+    cleanData: function() {
+      this.list=[];
     },
   },
 };
